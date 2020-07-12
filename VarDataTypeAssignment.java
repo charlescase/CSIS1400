2112704a73ef04b256cd11d603eeecdf1f6dfe8a
charlescase <68200347+charlescase@users.noreply.github.com>
2020-07-12 12:54:57 -0600

    Add files via upload

---
+++ VarDataTypeAssignment.java
@@ -0,0 +1,15 @@
+package variablesanddatatypes;
+
+public class VarDataTypeAssignment {
+	public static void main(String[] args) {
+		System.out.println("===============================");
+		System.out.println("STUDENT REPORT");
+		System.out.println("===============================");
+		System.out.print("Student Name:\t Charles Case\n");
+		System.out.printf("Age: %02d\t\t GPA: %3.1f\n", 21, 3.66);
+		System.out.print("Major: General Studies\n");
+		System.out.println("Class: Sophomore");
+		System.out.println("===============================");
+
+}
+}
